ceb0b9c9507e5d43ba2651e74c03fb7d54c0978d
Pooranjoy Bhattacharya <pooranjoyb2016@gmail.com>
2022-12-19 23:08:28 +0530

    Time: 3 ms (59.88%), Space: 6.5 MB (22.44%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,9 +1,13 @@
 class Solution {
-    public:
-        bool isSubsequence(string s, string t) 
-        {        
-            for(int x=t.size()-1; x>=0 && !s.empty(); x--)
-                if (t[x]==s[s.size()-1]) s.pop_back();   //treating subsequence string as a stack
-            return s.empty();
+public:
+    bool isSubsequence(string s, string t) {
+        int lenS = s.size(), lenT = t.size();
+        int val = 0;
+        for(int i = 0, j = 0; i < lenS, j < lenT;){
+            if(s[i] == t[j])    {i++; j++; val++;}
+            else    j++;
         }
+        if(val == lenS)    return true;
+        else            return false;
+    }
 };
